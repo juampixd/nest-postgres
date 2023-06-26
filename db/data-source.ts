5935ace4b135b44337b9c1edce8eeea1a114e946
Juan Pablo Nuñez <juanpablo.nunez@training.assuresoft.com>
2023-06-25 22:26:02 -0400

    Adding migrations

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,14 +1,16 @@
+import { DataBasesEnum } from 'src/enums/data-bases.enum';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
+  name: DataBasesEnum.POSTGRES,
+  type: DataBasesEnum.POSTGRES,
   host: 'db',
   port: 5432,
-  username: 'postgres',
-  password: 'nestpostgres',
-  database: 'nestdb',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true, // Set to false in production
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || 'nestpostgres',
+  database: process.env.DB_NAME || 'users',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/db/migrations/*.js'],
 };
 
 const dataSource = new DataSource(dataSourceOptions);
